6f65119723265d0fb1a90d78e4a3f94d2eee705b
bg1987 <bg1987@gmail.com>
2014-01-13 19:57:27 +0200

    full HumanStrategyCoverage

--- src/tests/HumanStrategyTest.java
+++ src/tests/HumanStrategyTest.java
@@ -2,6 +2,10 @@ package tests;
 
 import static org.junit.Assert.*;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
 import ooad4.connectfour.HumanStrategy;
 import ooad4.core.Board;
 import ooad4.core.Player;
@@ -14,28 +18,14 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class HumanStrategyTest {
-	private class mockBoard extends Board
-	{
 
-		public mockBoard(int rows, int columns) {
-			super(rows, columns);
-			// TODO Auto-generated constructor stub
-		}
-		
-	}
-	
-	private class mockPlayer extends Player
-	{
-
-		public mockPlayer(Strategy strategy) {
-			super(strategy);
-			// TODO Auto-generated constructor stub
-		}
-		
-	}
 	
 	
 	private HumanStrategy humanStrat;
+	private Board board;
+	private Player player;
+	
+	private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
 
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
@@ -47,11 +37,17 @@ public class HumanStrategyTest {
 
 	@Before
 	public void setUp() throws Exception {
-
+		board = new BoardMock(1, 1);
+		player = new PlayerMock();
+		
+		
 	}
 
 	@After
 	public void tearDown() throws Exception {
+		System.setIn(System.in);
+		System.setOut(System.out);
+		
 	}
 
 	@Test
@@ -69,15 +65,67 @@ public class HumanStrategyTest {
 	
 
 	@Test(expected=IllegalArgumentException.class)
-	public void testNextMove_NullParams() {
+	public void testNextMove_NullParams1() {
 		humanStrat = HumanStrategy.getInstance();
 		humanStrat.nextMove(null, null);
 	}
 	
-	@Test(expected=IllegalArgumentException.class)
+	
+	@Test
 	public void testNextMove_NonIntInput() {
-		humanStrat = HumanStrategy.getInstance();
+		humanStrat = HumanStrategy.getInstance(); //todo move to setup??
+		
+		System.setOut(new PrintStream(outContent));
+		ByteArrayInputStream in = new ByteArrayInputStream("a\r\n0\r\n".getBytes());
+		System.setIn(in);
+		
+		humanStrat.nextMove(board, player);
+
+		String s = outContent.toString();
+		assertEquals("What is your next move? [0-0]\r\nSorry, I didn't understand you. Please try again.\r\n", s);
 	}
+	
+	@Test
+	public void testNextMove_NegativeIntInput() {
+		humanStrat = HumanStrategy.getInstance(); //todo move to setup??
+		
+		System.setOut(new PrintStream(outContent));
+		ByteArrayInputStream in = new ByteArrayInputStream("-1\r\n0\r\n".getBytes());
+		System.setIn(in);
+		humanStrat.nextMove(board, player);
+		
+		String s = outContent.toString();
+		assertEquals("What is your next move? [0-0]\r\nSorry, that is not on the board\r\n", s);
+	}
+	
+	@Test
+	public void testNextMove_OutofBoundsIntInput() {
+		humanStrat = HumanStrategy.getInstance(); //todo move to setup??
+		
+		System.setOut(new PrintStream(outContent));
+		ByteArrayInputStream in = new ByteArrayInputStream("11\r\n0\r\n".getBytes());
+		System.setIn(in);
+		humanStrat.nextMove(board, player);
+		
+		String s = outContent.toString();
+		assertEquals("What is your next move? [0-0]\r\nSorry, that is not on the board\r\n", s);
+	}
+	
+	@Test
+	public void testNextMove_GoodInput() {
+		humanStrat = HumanStrategy.getInstance(); //todo move to setup??
+		
+		System.setOut(new PrintStream(outContent));
+		ByteArrayInputStream in = new ByteArrayInputStream("0\r\n".getBytes());
+		System.setIn(in);
+		humanStrat.nextMove(board, player);
+		
+		String s = outContent.toString();
+		assertEquals("What is your next move? [0-0]\r\n", s);
+	}	
+
+	
+	
 	
 	
 }
